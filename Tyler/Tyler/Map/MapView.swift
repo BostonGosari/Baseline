0ac7798b3dd43794b93fcfc142e92721a3c11610
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2023-10-09 00:11:12 +0900

    feat #5 : 초기 뷰 설정 및 초기뷰로 돌아가는 버튼 구현

--- Tyler/Tyler/Map/MapView.swift
+++ Tyler/Tyler/Map/MapView.swift
@@ -9,19 +9,19 @@ import SwiftUI
 import MapKit
 
 extension CLLocationCoordinate2D {
-    static let duckRun = CLLocationCoordinate2D(latitude: 37.54088, longitude: 127.07648)
+    static let duckRun = CLLocationCoordinate2D(latitude: 37.54161, longitude: 127.07648)
 }
 
 struct MapView: View {
     
-    @State private var position: MapCameraPosition = .automatic
+    @State private var position: MapCameraPosition = .camera(.init(centerCoordinate: .duckRun, distance: 1800.0, heading: -80.0))
     
     var body: some View {
         VStack {
             Text("Map 표시")
-            Map() {
+            Map(position: $position) {
                 UserAnnotation()
-                Annotation("Duck Run", coordinate: .duckRun) {
+                Annotation("오리런", coordinate: .duckRun) {
                     ZStack {
                         RoundedRectangle(cornerRadius: 10, style: .continuous)
                             .foregroundStyle(.yellow)
@@ -37,6 +37,26 @@ struct MapView: View {
                 MapUserLocationButton()
                 MapCompass()
                 MapScaleView()
+            }
+            .safeAreaInset(edge: .bottom) {
+                Button{
+                    withAnimation {
+                        position = .camera(.init(centerCoordinate: .duckRun, distance: 1800.0, heading: -80.0))
+                    }
+                } label: {
+                    Image("duck")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20)
+                        .padding(5)
+                        .background {
+                            RoundedRectangle(cornerRadius: 8, style: .circular)
+                                .foregroundStyle(.yellow)
+                        }
+                        .padding()
+                }
+                .frame(maxWidth: .infinity)
+                .background(.thinMaterial)
             }
             .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
             .frame(maxWidth: .infinity)
