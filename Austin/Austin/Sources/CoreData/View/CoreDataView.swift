2921286833cd9d34f716d5e507b73acbb5026e16
Moonkey <moonkey48@icloud.com>
2023-10-09 14:49:56 +0900

    feat #7 : CoreDataView 에서 CoreData 연동 및 CRUD 기능 구현

--- Austin/Austin/Sources/CoreData/View/CoreDataView.swift
+++ Austin/Austin/Sources/CoreData/View/CoreDataView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct CoreDataView: View {
+    @Environment(\.managedObjectContext) private var managedObjectContext
+    @FetchRequest(
+        entity: Person.entity(),
+        sortDescriptors: [
+            NSSortDescriptor(keyPath: \Person.age, ascending: true)
+        ]
+    ) private var people: FetchedResults<Person>
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    // MARK: 이후 ViewModel로 분리 필요
+    func saveContext() {
+        do {
+          try managedObjectContext.save()
+        } catch {
+          print("Error saving managed object context: \(error)")
+        }
+    }
+    
+    func addPerson(name: String, age: Int32) {
+        let newPerson = Person(context: managedObjectContext)
+
+        newPerson.name = name
+        newPerson.age = age
+
+        saveContext()
+    }
+
+    func deletePerson(at offsets: IndexSet) {
+      offsets.forEach { index in
+        let person = self.people[index]
+        self.managedObjectContext.delete(person)
+      }
+      saveContext()
     }
 }
 
