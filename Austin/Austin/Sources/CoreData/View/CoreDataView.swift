5a8e48f5c12e90e2098d4ffd56c2117108fff817
Moonkey <moonkey48@icloud.com>
2023-10-09 14:34:16 +0900

    add #7 : TestModel 파일 생성

---
+++ Austin/Austin/Sources/CoreData/View/CoreDataView.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataView.swift
+//  Austin
+//
+//  Created by Seungui Moon on 10/9/23.
+//
+
+import SwiftUI
+
+struct CoreDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataView()
+}
