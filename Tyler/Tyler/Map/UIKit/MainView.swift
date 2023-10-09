aab8402496f5d95742c819d03a93de1790f7303e
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2023-10-09 20:39:22 +0900

    refactor #5 : MainView 리팩토링

--- Tyler/Tyler/Map/UIKit/MainView.swift
+++ Tyler/Tyler/Map/UIKit/MainView.swift
@@ -9,24 +9,21 @@ import CoreLocation
 import MapKit
 import SwiftUI
 
-struct AnnotationItem: Identifiable {
-    let id = UUID()
-    let coordinate: CLLocationCoordinate2D
-    let title: String
-    let imageName: String
-}
-
 struct MainView: View {
     
+    // 위치 권한 관리
     let locationManager = CLLocationManager()
     
+    // Map 표시
     @State private var camera = MKMapCamera(lookingAtCenter: .duckRun, fromDistance: 1300, pitch: 0, heading: -80)
     
+    // 맵에 Pin 기능
     @State private var annotations: [AnnotationItem] = [
         AnnotationItem(coordinate: .duckRun, title: "오리런", imageName: "duck"),
         AnnotationItem(coordinate: CLLocationCoordinate2D(latitude: 37.53958, longitude: 127.07435), title: "스타트 포인트", imageName: "startpoint")
     ]
     
+    // 맵에 경로 표시
     @State private var coordinates: [CLLocationCoordinate2D] = {
         if let kmlFilePath = Bundle.main.path(forResource: "duckRun", ofType: "kml") {
             let kmlParser = KMLParser()
@@ -35,94 +32,88 @@ struct MainView: View {
         return []
     }()
     
+    // 지도 토글 관련
     @State var isMapChanged = false
+    
+    // 애니메이션 관련
     @State var currentStep = 0
     @State var timer: Timer?
     
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading){
             Text("MapView")
-            MapView(
-                camera: camera,
-                coordinates: coordinates,
-                annotations: annotations
-            )
+                .bold()
+            
+            MapView(camera: camera,
+                    coordinates: coordinates,
+                    annotations: annotations)
             .overlay(alignment: .bottomTrailing) {
-                HStack {
-                    Button {
-                        updateCamera()
-                    } label: {
-                        Image("duck")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 20)
-                            .padding(5)
-                            .background {
-                                RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                    .foregroundStyle(.yellow)
-                                    .shadow(radius: 2)
-                            }
-                    }
-                }
-                .padding()
+                duckButton
             }
             .frame(maxWidth: .infinity)
             .frame(height: 300)
             .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-            .padding(.horizontal, 30)
             .overlay {
                 if isMapChanged {
                     MapUserView()
                         .frame(maxWidth: .infinity)
                         .frame(height: 300)
                         .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                        .padding(.horizontal, 30)
                 }
             }
+            
             Button(isMapChanged ? "Map User View" : "Map View") {
-                withAnimation {
-                    isMapChanged.toggle()
-                }
+                isMapChanged.toggle()
             }
+            .frame(maxWidth: .infinity)
             .buttonStyle(.bordered)
             
             Text("Map Animation")
-            MapAnimationView(camera: camera, coordinates: coordinates, annotations: annotations, currentStep: currentStep)
-                .overlay(alignment: .bottomTrailing) {
-                    HStack {
-                        Button {
-                            updateCamera()
-                        } label: {
-                            Image("duck")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20)
-                                .padding(5)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                        .foregroundStyle(.yellow)
-                                        .shadow(radius: 2)
-                                }
-                        }
-                    }
-                    .padding()
-                }
-                .frame(maxWidth: .infinity)
-                .frame(height: 300)
-                .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                .padding(.horizontal, 30)
+                .bold()
+            MapAnimationView(camera: camera,
+                             coordinates: coordinates,
+                             annotations: annotations,
+                             currentStep: currentStep)
+            .overlay(alignment: .bottomTrailing) {
+                duckButton
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 300)
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            
             Button(currentStep == 0 ? "Animate" : "Animating...") {
                 withAnimation {
+                    updateCamera()
                     startAnimation()
                 }
             }
+            .frame(maxWidth: .infinity)
             .buttonStyle(.bordered)
             .disabled(currentStep != 0)
         }
         .onAppear {
             locationManager.requestWhenInUseAuthorization()
         }
+        .padding(.horizontal, 30)
+    }
+    
+    private var duckButton: some View {
+        Button {
+            updateCamera()
+        } label: {
+            Image("duck")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 20)
+                .padding(5)
+                .background {
+                    RoundedRectangle(cornerRadius: 5, style: .continuous)
+                        .foregroundStyle(.yellow)
+                        .shadow(radius: 2)
+                }
+        }
+        .padding()
     }
     
     private func updateCamera() {
@@ -131,13 +122,14 @@ struct MainView: View {
         }
     }
     
-    // 애니메이션 시작
     private func startAnimation() {
-        timer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { _ in
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { _ in
             if currentStep < coordinates.count {
                 currentStep += 1
             } else {
                 stopAnimation()
+                
+                /// 경로가 완성 된 후 1초 이후 경로가 사라집니다.
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                     currentStep = 0
                 }
@@ -145,14 +137,18 @@ struct MainView: View {
         }
     }
     
-    // 애니메이션 정지
     private func stopAnimation() {
         timer?.invalidate()
         timer = nil
     }
 }
 
-
+struct AnnotationItem: Identifiable {
+    let id = UUID()
+    let coordinate: CLLocationCoordinate2D
+    let title: String
+    let imageName: String
+}
 
 #Preview {
     MainView()
