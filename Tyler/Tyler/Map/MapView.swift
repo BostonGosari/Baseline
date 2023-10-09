a7b65e4f4d4468b9a0a8ad6eeb438064b6c5de7c
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2023-10-09 10:49:10 +0900

    feat #5 : iOS 17.0 이하에서 적용가능한 뷰 구현

--- Tyler/Tyler/Map/MapView.swift
+++ Tyler/Tyler/Map/MapView.swift
@@ -4,68 +4,119 @@
 //
 //  Created by Hyunjun Kim on 10/8/23.
 //
-
-import SwiftUI
+import CoreLocation
 import MapKit
+import SwiftUI
 
 extension CLLocationCoordinate2D {
-    static let duckRun = CLLocationCoordinate2D(latitude: 37.54161, longitude: 127.07648)
+    static let duckRun = CLLocationCoordinate2D(
+        latitude: 37.54161,
+        longitude: 127.07648
+    )
+}
+
+struct RunPoint: Identifiable {
+    let id = UUID()
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+    let image: String
 }
 
 struct MapView: View {
     
-    @State private var position: MapCameraPosition = .camera(.init(centerCoordinate: .duckRun, distance: 1800.0, heading: -80.0))
+    @State private var region = MKCoordinateRegion(
+        center: .duckRun,
+        latitudinalMeters: 600,
+        longitudinalMeters: 600
+    )
+    
+    @State private var region2 = MKCoordinateRegion(
+        center: .duckRun,
+        latitudinalMeters: 600,
+        longitudinalMeters: 600
+    )
+    
+    
+    @State private var camera = MKMapCamera(lookingAtCenter: .duckRun, fromDistance: 1800, pitch: 0, heading: -80)
+        
+    @State private var interactionMode: MapInteractionModes = .all
+    @State private var userTracking: MapUserTrackingMode = .none
+    
+    let locationManager = CLLocationManager()
+    
+    let duckRunPoints = [
+        RunPoint(title: "오리런", coordinate: .duckRun, image: "duck"),
+        RunPoint(title: "스타트 포인트", coordinate: CLLocationCoordinate2D(latitude: 37.53958, longitude: 127.07435), image: "startpoint")
+    ]
     
     var body: some View {
-        VStack {
-            Text("Map 표시")
-            Map(position: $position) {
-                UserAnnotation()
-                Annotation("오리런", coordinate: .duckRun) {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10, style: .continuous)
-                            .foregroundStyle(.yellow)
+        VStack(spacing: 30) {
+            Map(coordinateRegion: $region,
+                interactionModes: interactionMode,
+                showsUserLocation: true,
+                userTrackingMode: $userTracking,
+                annotationItems: duckRunPoints,
+                annotationContent: { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    if location.title == "오리런" {
+                        VStack {
+                            Image(location.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30)
+                            Text(location.title)
+                                .font(.caption2)
+                                .foregroundStyle(.secondary)
+                        }
+                    } else {
+                        VStack {
+                            Image(location.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 15)
+                            Text(location.title)
+                                .font(.system(size: 6))
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                }
+            })
+            .overlay(alignment: .bottomTrailing) {
+                HStack {
+                    Button {
+                        withAnimation {
+                            region = MKCoordinateRegion(
+                                center: .duckRun,
+                                latitudinalMeters: 600,
+                                longitudinalMeters: 600
+                            )
+                        }
+                    } label: {
                         Image("duck")
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 30)
+                            .frame(width: 20)
                             .padding(5)
+                            .background {
+                                RoundedRectangle(cornerRadius: 5, style: .continuous)
+                                    .foregroundStyle(.yellow)
+                                    .shadow(radius: 2)
+                            }
                     }
                 }
+                .padding()
             }
-            .mapControls {
-                MapUserLocationButton()
-                MapCompass()
-                MapScaleView()
-            }
-            .safeAreaInset(edge: .bottom) {
-                Button{
-                    withAnimation {
-                        position = .camera(.init(centerCoordinate: .duckRun, distance: 1800.0, heading: -80.0))
-                    }
-                } label: {
-                    Image("duck")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20)
-                        .padding(5)
-                        .background {
-                            RoundedRectangle(cornerRadius: 8, style: .circular)
-                                .foregroundStyle(.yellow)
-                        }
-                        .padding()
-                }
-                .frame(maxWidth: .infinity)
-                .background(.thinMaterial)
-            }
-            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
             .frame(maxWidth: .infinity)
-            .frame(height: 450)
+            .frame(height: 300)
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
             .padding(.horizontal, 30)
-            Text("hello world")
+        }
+        .onAppear {
+            locationManager.requestWhenInUseAuthorization()
         }
     }
 }
+
 
 #Preview {
     MapView()
