7f6641799f4127c7004a41aa19a3fa9dd1d693e0
tyler <120548537+hyunjuntyler@users.noreply.github.com>
2023-10-10 19:25:44 +0900

    feat #12 : watchOS 프로젝트 생성

---
+++ Tyler/WatchTyler Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchTyler Watch App
+//
+//  Created by Hyunjun Kim on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tyler/WatchTyler Watch App/WatchTylerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTylerApp.swift
+//  WatchTyler Watch App
+//
+//  Created by Hyunjun Kim on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchTyler_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
